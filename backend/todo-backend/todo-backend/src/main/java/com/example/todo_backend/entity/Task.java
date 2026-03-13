e2afad0d21ba95d5ecb1c0765142dfe2fdf55847
sahan1997dilshan <dsahan548@gmail.com>
2026-03-14 03:14:00 +0530

    Added Task entity

---
+++ backend/todo-backend/todo-backend/src/main/java/com/example/todo_backend/entity/Task.java
@@ -0,0 +1,35 @@
+package com.example.todo_backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "task")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
